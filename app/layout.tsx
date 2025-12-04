df04e0fda13c35d5a705006248ebe9d1f787f7ec
Alwin Personal <alwinabyofficial@gmail.com>
2025-12-04 14:59:55 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local"
 
+
+const star = localFont({
+  src:"/fonts/Starjedi.ttf",
+  variable:"--font-star"
+
+})
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${star.variable} antialiased`}
       >
         {children}
       </body>
